6423c6d144159c1d2ba061cd616270331b9cc147
RohitXopBolta <rohitpapop766@gmail.com>
2025-07-27 18:49:25 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -17,13 +17,13 @@ def is_enabled(value, default):
 
 
 class Config(object):
-    API_ID = int(os.environ.get("API_ID", ""))
-    API_HASH = os.environ.get("API_HASH", "")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
+    API_ID = int(os.environ.get("API_ID", "26548330"))
+    API_HASH = os.environ.get("API_HASH", "244c3afd019c7b0cd1a8184cd2be2495")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7663652095:AAEdnuTlHTBHPZSSgQOp_ZDlQagJAK53Sng")
     DATABASE_NAME = os.environ.get("DATABASE_NAME", "tg_bot")
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    OWNER_ID = int(os.environ.get("OWNER_ID", ""))
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", ""))
+    DATABASE_URL = os.environ.get("DATABASE_URL", "mongodb+srv://FLAMEXMASTER1:FLAMEXMASTER1@cluster0.gsbo8hg.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    OWNER_ID = int(os.environ.get("OWNER_ID", "6890753169"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002823882872"))
     WEB_SERVER = is_enabled(os.environ.get("WEB_SERVER", "False"), False)
     WEBHOOK = True  # Don't change this
     PORT = int(os.environ.get("PORT", 8000))
